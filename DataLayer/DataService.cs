9ea8c10ea6bfeb4155185f65583853bc3b06b509
Sebastianrix <sebastianrix11@gmail.com>
2024-10-16 14:57:42 +0200

    Added functionality to retrieve from categories with ID

--- DataLayer/DataService.cs
+++ DataLayer/DataService.cs
@@ -1,5 +1,23 @@
-﻿namespace DataLayer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer;
 
 public class DataService
-{
+{   
+    public IList<Category> GetCategories  () {
+        var db = new NorthwindContext();
+        return db.Categories.ToList();
+    }
+    public Category GetCategory(int requested_ID)
+    {
+        var db = new NorthwindContext();
+
+        var myCategory = db.Categories.FirstOrDefault(c => c.Id == requested_ID);
+        return myCategory;
+    }
+
 }
