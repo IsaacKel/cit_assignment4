cc8784a05275d166a190bb2ace335c5cdcfe270a
Sebastianrix <sebastianrix11@gmail.com>
2024-10-17 11:37:58 +0200

    Retrieve product by category ID functionality added

--- DataLayer/Product.cs
+++ DataLayer/Product.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer;
-public class Product
+namespace DataLayer
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    
-    public double UnitPrice { get; set; }
-    public string? QuantityPerUnit { get; set; }
-    public int UnitsInStock { get; set; }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double UnitPrice { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public string? CategoryName { get; set; }
 
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
+    }
 
 }
