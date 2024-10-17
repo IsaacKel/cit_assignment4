ef2e8aae5b17298da0cf51db5957a0a8056eb2be
Sebastianrix <sebastianrix11@gmail.com>
2024-10-17 20:22:17 +0200

    Added Order class and Order mapping

--- DataLayer/NorthwindContext.cs
+++ DataLayer/NorthwindContext.cs
@@ -23,6 +23,7 @@ internal class NorthwindContext : DbContext
     {
         MapCategories(modelBuilder);
         MapProducts(modelBuilder);
+        MapOrders(modelBuilder);
     }
 
     private static void MapCategories(ModelBuilder modelBuilder)
@@ -47,5 +48,26 @@ internal class NorthwindContext : DbContext
         modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
         modelBuilder.Entity<Product>().HasOne(x => x.Category).WithMany().HasForeignKey(x => x.CategoryId);
         modelBuilder.Entity<Product>().Ignore(x => x.CategoryName);
+    }
+
+    private static void MapOrders(ModelBuilder modelBuilder)
+    {
+        // Orders
+        modelBuilder.Entity<Order>().ToTable("orders");
+        modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+        modelBuilder.Entity<Order>().Property(x => x.CustomerId).HasColumnName("customerid");
+        modelBuilder.Entity<Order>().Property(x => x.EmployeeId).HasColumnName("employeeid");
+        modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
+        modelBuilder.Entity<Order>().Property(x => x.Required).HasColumnName("requireddate");
+        modelBuilder.Entity<Order>().Property(x => x.ShippedDate).HasColumnName("shippeddate");
+        modelBuilder.Entity<Order>().Property(x => x.ShipVia).HasColumnName("shipvia");
+        modelBuilder.Entity<Order>().Property(x => x.Freight).HasColumnName("freight");
+        modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
+        modelBuilder.Entity<Order>().Property(x => x.ShipAddress).HasColumnName("shipaddress");
+        modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
+        modelBuilder.Entity<Order>().Property(x => x.ShipRegion).HasColumnName("shipregion");
+        modelBuilder.Entity<Order>().Property(x => x.ShipPostalCode).HasColumnName("shippostalcode");
+        modelBuilder.Entity<Order>().Property(x => x.ShipCountry).HasColumnName("shipcountry");
+        //modelBuilder.Entity<Order>().HasOne(x => x.Customer).WithMany().HasForeignKey(x => x.CustomerId);
     }
 }
