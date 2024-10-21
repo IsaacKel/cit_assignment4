eee5a7dd92b2d9e2a0b12c5515fdcd89feb51de9
Sebastianrix <sebastianrix11@gmail.com>
2024-10-21 20:12:07 +0200

    Added wepservice and datalyaer. Solution pass all tests in part 1 and part 2

---
+++ CIT_13_Assignment4/DataLayer/IDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer;
+public interface IDataService
+{
+    IList<Category> GetCategories();
+    Category? GetCategory(int id);
+    Category? CreateCategory(string name, string description);
+    bool DeleteCategory(int id);
+    bool UpdateCategory(int id, string name, string description);
+
+    Product? GetProduct(int id);
+    IList<Product> GetProductByCategory(int categoryId);
+    IList<Product> GetProductByName(string name);
+    Order? GetOrder(int id);
+
+    IList<Order> GetOrders();
+    IList<OrderDetails>? GetOrderDetailsByOrderId(int id);
+    IList<OrderDetails>? GetOrderDetailsByProductId(int id);
+
+}
